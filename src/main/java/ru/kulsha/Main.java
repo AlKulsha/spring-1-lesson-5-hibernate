c463c26732a55356f3d90a97a012cf5ddaf8a92a
AlKulsha <alenabat18@gmail.com>
2022-11-17 06:21:49 +0300

    Initial commit

---
+++ src/main/java/ru/kulsha/Main.java
@@ -0,0 +1,39 @@
+package ru.kulsha;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class Main {
+    private static SessionFactory factory;
+
+    public static void init(){
+        factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    public static void shutdown(){
+        if(factory != null){
+            factory.close();
+        }
+    }
+
+
+    public static void main(String[] args) {
+      try{
+          init();
+          Session session = factory.getCurrentSession();
+          session.beginTransaction();
+          User oldUser = session.get(User.class, 1L);
+          oldUser.print();
+          session.getTransaction().commit();
+      } catch (Exception e){
+          e.printStackTrace();
+      } finally {
+          shutdown();
+      }
+    }
+}
